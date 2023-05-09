5275374a6dd3dc24b4c38158863aff4fe7f56a23
kuning174 <bbeelzebub48@gmail.com>
2023-05-09 11:19:36 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\OrderController;
+
+Route::get('/', function(){
+    return view('auth.login');
+});
+
+Auth::routes();
+Route::match(["GET", "POST"], "/register", function(){
+    return redirect("/login");
+})->name("register");
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource("users", UserController::class);
+Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
+Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
+Route::resource('categories', CategoryController::class);
+Route::get('/ajax/categories/search', [CategoryController::class, 'ajaxSearch']);
+Route::resource('books', BookController::class);
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('orders', OrderController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
